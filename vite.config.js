d23db6fa1814799227bd92feeb8e94556429cabc
bi8bo846 <bi8bobo88@gmail.com>
2026-06-03 06:53:49 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
+    outDir: "dist",
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"]
+        }
+      }
+    }
   }
 })
