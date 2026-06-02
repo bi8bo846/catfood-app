a1da098601ddbdc8c195cabe9c39f90aec5394f7
bi8bo846 <bi8bobo88@gmail.com>
2026-06-03 07:31:02 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./RonRonner.jsx"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement(App, null)
+)
